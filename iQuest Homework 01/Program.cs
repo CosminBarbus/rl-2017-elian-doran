057dd1245ff9358650d700c544d7457ccba6606a
Elian Doran <elinnum@gmail.com>
2017-11-22 15:40:35 +0200

    Print the required console message

--- iQuest Homework 01/Program.cs
+++ iQuest Homework 01/Program.cs
@@ -8,11 +8,22 @@ namespace ElianDoran.iQuest.Homework01
 {
     class Program
     {
+        static void Pause()
+        {
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
             List<Animal> animals = Animal.GetAllAnimals();
 
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(String.Format("The {0} makes {1}", animal.Name, animal.Sound));
+            }
 
+            Pause();
         }
     }
 }
