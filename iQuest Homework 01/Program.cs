d2fc65d02b44a124cb98442ca07207ab6fd6366d
Elian Doran <elinnum@gmail.com>
2017-11-22 15:48:56 +0200

    Add Speech Synthesis

--- iQuest Homework 01/Program.cs
+++ iQuest Homework 01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace ElianDoran.iQuest.Homework01
 {
@@ -16,14 +17,29 @@ namespace ElianDoran.iQuest.Homework01
 
         static void Main(string[] args)
         {
-            List<Animal> animals = Animal.GetAllAnimals();
-
-            foreach (var animal in animals)
+            using (var synthesizer = new SpeechSynthesizer())
             {
-                Console.WriteLine(String.Format("The {0} makes {1}", animal.Name, animal.Sound));
+                synthesizer.Volume = 100;
+                synthesizer.SpeakProgress += Synthesizer_SpeakProgress;
+
+                List<Animal> animals = Animal.GetAllAnimals();
+
+                foreach (var animal in animals)
+                {
+                    var phrase = String.Format("The {0} makes {1}", animal.Name, animal.Sound);
+
+                    synthesizer.Speak(phrase);
+
+                    Console.WriteLine();
+                }
             }
 
             Pause();
+        }
+
+        private static void Synthesizer_SpeakProgress(object sender, SpeakProgressEventArgs e)
+        {
+            Console.Write("{0} ", e.Text);
         }
     }
 }
