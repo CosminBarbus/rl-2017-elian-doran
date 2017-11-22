1b3d8fe6b6cf59bb61d18209e574ac67235ec0a3
Elian Doran <elinnum@gmail.com>
2017-11-22 15:35:49 +0200

    Add two animals

---
+++ iQuest Homework 01/Animals/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElianDoran.iQuest.Homework01.Animals
+{
+    class Cat: Animal
+    {
+        public override string Name => "Cat";
+
+        public override string Sound => "meow";
+    }
+}
---
+++ iQuest Homework 01/Animals/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElianDoran.iQuest.Homework01.Animals
+{
+    class Dog: Animal
+    {
+        public override string Name => "Dog";
+
+        public override string Sound => "woof";
+    }
+}
